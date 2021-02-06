cf4d3eafb2534f11bc404ed6edc33f8a0b51f7e1
Yann BOYER <yann.boyer742@gmail.com>
2021-02-06 19:52:49 +0100

    Some changes

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import pygame
+from pygame.locals import *
+
 pygame.init()
 
-pygame.display.init()
 screen = pygame.display.set_mode(size=(1280, 720))
 pygame.display.set_caption("Game_Project")
 running = True
 
 while running:
+	for event in pygame.event.get():
+		if event.type == QUIT:
+			running = False
+
 
 	pygame.display.flip()
-
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			running = False
